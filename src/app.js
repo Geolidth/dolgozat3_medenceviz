89038542cffc3d6c2b07bb9b0565317f557e413c
Geolidth <myyouz74@gmail.com>
2023-06-06 17:51:47 +0200

    Init

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const height = document.querySelector('#height');
+const weight = document.querySelector('#weight');
+const lenght = document.querySelector('#lenght');
+const value = document.querySelector('#value');
+
+$('calcbutton').on('click',function(){
+    
+});
