b64d7a841437c8925facc1773f981cbb7a682d23
Geolidth <myyouz74@gmail.com>
2023-06-06 18:16:14 +0200

    Működö számítás

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
-const height = document.querySelector('#height');
-const weight = document.querySelector('#weight');
-const lenght = document.querySelector('#lenght');
-const value = document.querySelector('#value');
 
-$('calcbutton').on('click',function(){
+
+$(window).on('load',function(){
+    $('#calcbutton').on('click',function(){
+        calcValue();
+    });
     
-});
+})
+
+
+function calcValue(){
+    let height = Number($('#height').val());
+    let weight = Number($('#weight').val());
+    let lenght = Number($('#lenght').val());
+    let correctHeight = height - 10;
+    let value = correctHeight*weight*lenght/1000000;
+    $('#value').val(value);
+    $('#height').val('');
+    $('#weight').val('');
+    $('#lenght').val('');
+    
+}
